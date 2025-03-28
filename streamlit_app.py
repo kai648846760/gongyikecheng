8d88356c825b8af2386fda86871c3128ada161ae
Loki <53172671+kai648846760@users.noreply.github.com>
2025-03-28 12:35:19 +0000

    copy

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,40 @@
 import streamlit as st
+import requests
 
-st.title("🎈 My new app")
+def get_people():
+    headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json',
+        'Origin': 'https://lianmeng.hetao101.com',
+        'Referer': 'https://lianmeng.hetao101.com/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0',
+        'authorization': st.secrets['token'],
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Microsoft Edge";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+    }
+
+    json_data = {
+        'team_id': '12628952',
+        'b2c_term_tag_names': [
+            '2025年春07期',
+        ],
+    }
+
+    response = requests.post(
+        'https://api.hetao101.com/ug-agent-back/v1/token/star/mobile/btoc/term/sumOnline',
+        headers=headers,
+        json=json_data,
+    )
+    return response.json()
+
+
+st.title("🎈 公益课程人数展示")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    f"当前人数：{get_people()['data']["get_account_num_str"]}"
 )
