1d135ecf318099037558bb25412c3df9d8f0927f
Loki <53172671+kai648846760@users.noreply.github.com>
2025-03-28 12:52:56 +0000

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,34 @@
 import streamlit as st
 import requests
 
-def get_people():
+def get_xueqi():
+    headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
+        'Connection': 'keep-alive',
+        'Content-Type': 'application/json',
+        'Origin': 'https://lianmeng.hetao101.com',
+        'Referer': 'https://lianmeng.hetao101.com/',
+        'Sec-Fetch-Dest': 'empty',
+        'Sec-Fetch-Mode': 'cors',
+        'Sec-Fetch-Site': 'same-site',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36 Edg/134.0.0.0',
+        'authorization': st.secrets['token'],
+        'sec-ch-ua': '"Chromium";v="134", "Not:A-Brand";v="24", "Microsoft Edge";v="134"',
+        'sec-ch-ua-mobile': '?0',
+        'sec-ch-ua-platform': '"macOS"',
+    }
+
+    params = ''
+
+    response = requests.get(
+        'https://api.hetao101.com/ug-agent-back/v1/token/star/mobile/btoc/terms',
+        params=params,
+        headers=headers,
+    )
+    return response.json()['data']
+
+def get_people(xueqi):
     headers = {
         'Accept': 'application/json, text/plain, */*',
         'Accept-Language': 'zh-CN,zh;q=0.9,en;q=0.8',
@@ -22,7 +49,7 @@ def get_people():
     json_data = {
         'team_id': '12628952',
         'b2c_term_tag_names': [
-            '2025年春07期',
+            xueqi,
         ],
     }
 
@@ -33,10 +60,10 @@ def get_people():
     )
     return response.json()
 
-people = get_people()['data']
+people_list = [ get_people(x['termTagName'])['data'] for x in get_xueqi() ]
 
 st.title("🎈 公益课程人数展示")
-for i in people:
+for i in people_list:
     st.write(
         f"当前季度 {i['b2c_term_tag_name']} .  该季度对应人数：{i['get_account_num_str']} 人"
     )
